fc4374930fb070bee899dba2ecdc21ee78064304
Azka <abdullasiddique027@gmail.com>
2026-04-01 20:32:37 +0530

    feat: Add terms page

---
+++ FrontEnd/src/pages/terms.jsx
@@ -0,0 +1,46 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+export default function Terms() {
+    return (
+
+        <div>
+            <Header />
+
+            <div>
+                <h1>Splitwise Terms of Service</h1>
+                <h3>Last updated: February 8th, 2024</h3>
+                <p>Splitwise is a shared ledger for friends, colleagues and family to keep track of expenses. Bills, IOUs, debts and payments recorded on Splitwise are informal records, and not legally binding contracts of some kind. We offer the Splitwise service "as-is" without a warranty. Splitwise® is a work-in-progress, so some features of our website and apps may not work exactly as planned. We may stop offering or restrict certain services or features at any time. Please be patient with us and we'll always do our best to support you.</p>
+
+                <p>We will modify our Terms Of Use and Privacy Policy periodically and post the most current version on this webpage, https://www.splitwise.com/terms. Here are the policies:</p>
+
+                <h2>Terms Of Use</h2>
+
+                <ul className="list-disc ml-4 mt-2">
+                    <li><span>Who can use Splitwise:</span>You may use our services only if you agree to these Terms of Service and to the processing of your information as described in our Privacy Policy. You may not use Splitwise if you are barred from receiving services under the laws of the applicable jurisdiction, if we previously banned your account for violation of these terms, or if you are under the age of 13.</li>
+                    <li><span>Description:</span>You may use our services only if you agree to these Terms of Service and to the processing of your information as described in our Privacy Policy. You may not use Splitwise if you are barred from receiving services under the laws of the applicable jurisdiction, if we previously banned your account for violation of these terms, or if you are under the age of 13.</li>
+                    <li><span>Informal debts:</span>You may use our services only if you agree to these Terms of Service and to the processing of your information as described in our Privacy Policy. You may not use Splitwise if you are barred from receiving services under the laws of the applicable jurisdiction, if we previously banned your account for violation of these terms, or if you are under the age of 13.</li>
+                    <li><span>Conditions of use:</span>You may use our services only if you agree to these Terms of Service and to the processing of your information as described in our Privacy Policy. You may not use Splitwise if you are barred from receiving services under the laws of the applicable jurisdiction, if we previously banned your account for violation of these terms, or if you are under the age of 13.</li>
+                    <li><span>What you represent:</span>You may use our services only if you agree to these Terms of Service and to the processing of your information as described in our Privacy Policy. You may not use Splitwise if you are barred from receiving services under the laws of the applicable jurisdiction, if we previously banned your account for violation of these terms, or if you are under the age of 13.</li>
+                    <li><span>No warranty:</span>You may use our services only if you agree to these Terms of Service and to the processing of your information as described in our Privacy Policy. You may not use Splitwise if you are barred from receiving services under the laws of the applicable jurisdiction, if we previously banned your account for violation of these terms, or if you are under the age of 13.</li>
+                    <li><span>Termination and Refusal of Service: </span>You may use our services only if you agree to these Terms of Service and to the processing of your information as described in our Privacy Policy. You may not use Splitwise if you are barred from receiving services under the laws of the applicable jurisdiction, if we previously banned your account for violation of these terms, or if you are under the age of 13.</li>
+                    <li><span>User Posted Content:</span>You may use our services only if you agree to these Terms of Service and to the processing of your information as described in our Privacy Policy. You may not use Splitwise if you are barred from receiving services under the laws of the applicable jurisdiction, if we previously banned your account for violation of these terms, or if you are under the age of 13.</li>
+                    <li><span>Splitwise Content Standards:</span>
+                    <ul className="list-disc ml-4 mt-2">
+                        <li>You do not have the right to share, or that is invasive of another person's right of privacy or right of publicity. You may not share content that includes personal information meant to harass, blackmail, or otherwise target another person.</li>
+                        <li>Is false, misleading, or deceptive. For example, you cannot change your User Avatar to misrepresent yourself as another person in a harmful way.</li>
+                    </ul>
+                    </li>
+
+
+                    <li><span>Privacy and Security:</span>You may use our services only if you agree to these Terms of Service and to the processing of your information as described in our Privacy Policy. You may not use Splitwise if you are barred from receiving services under the laws of the applicable jurisdiction, if we previously banned your account for violation of these terms, or if you are under the age of 13.</li>
+                    <li><span>Termination and Refusal of Service: </span>You may use our services only if you agree to these Terms of Service and to the processing of your information as described in our Privacy Policy. You may not use Splitwise if you are barred from receiving services under the laws of the applicable jurisdiction, if we previously banned your account for violation of these terms, or if you are under the age of 13.</li>
+
+                </ul>
+            </div>
+
+            <Footer/>
+        </div>
+
+    )
+}
